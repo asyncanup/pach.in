86d7563223a42dd2c8c9421ae04ca6c9733b0453
Anup Bishnoi <pixelsallover@gmail.com>
2014-03-16 21:52:26 +0530

    Updating data on firebase now reflects in local copies

--- data.js
+++ data.js
@@ -1,30 +1,58 @@
 var Firebase = require("firebase"),
-    _ = require("underscore");
+    _ = require("underscore"),
+    debug = require("debug")("pach.in");
     
 var pkg = require("./package.json"),
     db = new Firebase(process.env.FIREBASE).child(pkg.name.replace(".", "_"));
     
 var data = {};
 
-["posters", "chronicles", "subscriptions", "meets"].forEach(function (item) {
-    data[item] = {};
-    data[item].local = [];
-    data[item].cloud = db.child(item);
+["posters", "chronicles", "subscriptions", "meets"].forEach(function (type) {
+    data[type] = {};
+    data[type].local = [];
+    data[type].cloud = db.child(type);
     
-    data[item].cloud.on("child_added", function (snap) {
-        data[item].local.push(snap.val());
+    data[type].cloud.on("child_added", function (snap) {
+        data[type].local.push(snap.val());
     });
     
-    data[item].cloud.on("child_removed", function (snap) {
+    data[type].cloud.on("child_removed", function (snap) {
+        var localItems = data[type].local,
+            localItemIndex = findLocalItemIndex(snap.val(), localItems, type);
+        
+        if (~localItemIndex) {
+            debug("Removed " + type + ": " + JSON.stringify(localItemIndex));
+            localItems.splice(localItemIndex, 1);
+        } else {
+            debug("Removed " + type + " not in local: " + JSON.stringify(remoteItem));
+        }
+    });
+    
+    data[type].cloud.on("child_changed", function (snap) {
         var remoteItem = snap.val(),
-            localItems = data[item].local;
-        
-        var localItemFound = _.find(localItems, function (localItem) {
-            return _.isEqual(localItem, remoteItem);
-        });
-        
-        localItems.splice(localItems.indexOf(localItemFound), 1);
+            localItems = data[type].local,
+            localItemIndex = findLocalItemIndex(remoteItem, localItems, type);
+
+        if (~localItemIndex) {
+            debug("Changed " + type + ": " + JSON.stringify(localItemIndex));
+            localItems[localItemIndex] = remoteItem;
+        } else {
+            debug("Changed " + type + " doesn't exist in local: " + JSON.stringify(remoteItem));
+        }
     });
 });
+
+function findLocalItemIndex(remoteItem, localItems, type) {
+    var idAttributeForType = {
+        "subscriptions": "email",
+        "meets": "meet_id",
+        "posters": "meet_id",
+        "chronicles": "meet_id"
+    };
+
+    var idAttribute = idAttributeForType[type];
+
+    return _.pluck(localItems, idAttribute).indexOf(remoteItem[idAttribute]);
+}
 
 module.exports = data;
